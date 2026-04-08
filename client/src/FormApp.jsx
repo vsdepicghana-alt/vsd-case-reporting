9b09fb44b2636ea608d5b7bc80ddcc2e5539405a
nryeboah <randyntimyeboah@gmail.com>
2026-04-08 11:58:53 +0000

    fix: CVL recepient email change

--- client/src/FormApp.jsx
+++ client/src/FormApp.jsx
@@ -157,7 +157,7 @@ function FormApp() {
       "Takoradi Veterinary Lab": "oseibright172@gmail.com",
       "Kumasi Veterinary Lab": "randyntimyeboah@gmail.com",
       "Accra Veterinary Lab": "kutamemark@gmail.com",
-      "Central Veterinary Lab Pong Tamale": "davidmedulla5@gmail.com",
+      "Central Veterinary Lab Pong Tamale": "jefferycrown@gmail.com",
     };
 
     const to_email = labEmails[caseRecord.selectedLab];
